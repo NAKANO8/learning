98937d9cde4054f72958565e86adf3a911a19420
NAKANO8 <hika110308@gmail.com>
2025-09-30 22:41:51 +0900

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+const userInput = '10';
+calcResult = Number(userInput) + 10;
+calcResult = parseInt(userInput) + 10;
+calcResult = parseFloat(userInput) + 10;
+console.log(calcResult);
+
+const tenNumber = 10;
+calcResult = '10' + String(tenNumber);
+calcResult = '10' + tenNumber.toString();
+console.log(calcResult);
+
+function addResult(num1,num2) {
+  return num1 + num2;
+}
+
+const lastResult = addResult(1,2);
+console.log(lastResult);
