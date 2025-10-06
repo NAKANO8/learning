24ad2fcb656f7f9c2108906a43cdb5e13e6bddfa
NAKANO8 <hika110308@gmail.com>
2025-10-06 19:53:47 +0900

    container

--- javascript/main.js
+++ javascript/main.js
@@ -1,40 +1,57 @@
-const userInput = '10';
-calcResult = Number(userInput) + 10;
-calcResult = parseInt(userInput) + 10;
-calcResult = parseFloat(userInput) + 10;
-console.log(calcResult);
+// const userInput = '10';
+// calcResult = Number(userInput) + 10;
+// calcResult = parseInt(userInput) + 10;
+// calcResult = parseFloat(userInput) + 10;
+// console.log(calcResult);
 
-const tenNumber = 10;
-calcResult = '10' + String(tenNumber);
-calcResult = '10' + tenNumber.toString();
-console.log(calcResult);
+// const tenNumber = 10;
+// calcResult = '10' + String(tenNumber);
+// calcResult = '10' + tenNumber.toString();
+// console.log(calcResult);
 
-function addResult(num1,num2) {
-  return num1 + num2;
-}
+// function addResult(num1,num2) {
+//   return num1 + num2;
+// }
 
-const lastResult = addResult(1,2);
-console.log(lastResult);
+// const lastResult = addResult(1,2);
+// console.log(lastResult);
 
-const hello = 'hello';
-console.log(hello);
+// const hello = 'hello';
+// console.log(hello);
+// ok = 0;
+// if (ok) {
+//   console.log(ok);
+// } else {
+//   console.log('not ok');
+// }
+// //左がtruthyなら右側を返す
+// //左がfalseyなら左側を返す
+// ok = false && false;
+// console.log(ok);
 
-ok = 0;
-if (ok) {
-  console.log(ok);
-} else {
-  console.log('not ok');
-}
-//左がtruthyなら右側を返す
-//左がfalseyなら左側を返す
-ok = false && false;
+// //左がtruthyなら左側を返す
+// //左がfalseyなら右側を返す
+// ok = true || false;
+// console.log(ok);
+
+// const userInputNothing = '';
+// const userName = userInput || 'User';
+// console.log(userName)
+
+// function add(num1,num2) {
+//   console.log(num1 + num2);
+// }
+
+// add(1,2);
+// add(1,2);
+// add(1,2);
+// let watchedValue = 1;
+// watchedValue = 2;
+// watchedValue = 3;
+// console.log(watchedValue);
+
+// let userName2 = '' ?? 'User';
+// console.log(userName2);
+
+let ok = !!15;
 console.log(ok);
-
-//左がtruthyなら左側を返す
-//左がfalseyなら右側を返す
-ok = true || false;
-console.log(ok);
-
-const userInputNothing = '';
-const userName = userInput || 'User';
-console.log(userName)
