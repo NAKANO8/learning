25e348707e5ef18a38226cbfc6b9da8a99fae6d6
NAKANO8 <hika110308@gmail.com>
2025-10-06 23:04:45 +0900

    try-catch

--- javascript/main.js
+++ javascript/main.js
@@ -53,5 +53,124 @@
 // let userName2 = '' ?? 'User';
 // console.log(userName2);
 
-let ok = !!15;
-console.log(ok);
+// let ok = !!15;
+// console.log(ok);
+
+// {
+//   const hello = 'hello';
+//   console.log(hello);
+// }
+// const hello = 'hello2';
+// {
+//   console.log(hello);
+//   // const hello = 'hello1';
+// }
+
+// ok = 0;
+// if (ok) {
+//   console.log(ok);
+// } else if (ok === 0 ) {
+//   console.log('umm..ok!');
+// } else {
+//   console.log('Not OK');
+// }
+// ok = '';
+// ok = ok ? 'OK' : 'NO';
+// console.log(ok)
+
+// function vegetableColor(vegetable) {
+//   switch (vegetable) {
+//     case 'tomato':
+//     case 'paplika': {
+//       const message = `${vegetable} is red`;
+//       console.log(message);
+//       break;
+//     }
+//     case 'banana': {
+//       const message = `${vegetable} is yellow`;
+//       console.log(message);
+//       break;
+//     }
+//     case 'green pepper': {
+//       const message = `${vegetable} is green`;
+//       console.log(message);
+//       break;
+//     }
+//     default: {
+//       console.log('unknown');
+//     }
+//   }
+
+  // if (vegetable === 'tomato') {
+  //   console.log('red');
+  // } else if (vegetable === 'banana') {
+  //   console.log('yellow');
+  // } else if (vegetable === 'green pepper') {
+  //   console.log('green');
+  // } else {
+  //   console.log('unknown');
+  // }
+// }
+
+// vegetableColor('tomato');
+// vegetableColor('banana');
+// vegetableColor('green pepper');
+// vegetableColor('onion');
+// vegetableColor('paplika');
+// let i = 0;
+// while (i < 10) {
+//   let count = i + 1;
+//   console.log(`ループ${count}回目`);
+//   i += 1;
+// }
+let tomatoCount = 0;
+do {
+  tomatoCount += 1;
+  console.log(`トマトを${tomatoCount}個買いました`);
+} while (tomatoCount < 10);
+
+const Array = ['apple','banana','orange'];
+for (let i = 0;i < Array.length;i += 1) {
+  console.log(Array[i]);
+}
+
+let a = 'a', b = 'b',c;
+c = 'c';
+console.log(a,b,c); 
+
+const fruits = ['apple','banana','orange'];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+const char = ['お','に','ぎ','り'];
+for (const c of char) {
+  console.log(c);
+}
+const coffee = {
+  type: 'latte',
+  price: 400,
+  size: 'tall'
+};
+for (const key in coffee) {
+  console.log(`${key}: ${coffee[key]}`);
+}
+// array of
+// object in
+// break はループの終了
+// continue は次のループへ
+// returnは関数の終了、元に戻る
+// 最も内側のループから逃れる
+
+
+function printFruits() {
+  try {
+    console.log('try');
+    throw 'error';
+  } catch (error) {
+    console.log('エラーが発生しました');
+  } finally {
+    console.log('finally');
+  } // エラーが発生してもしなくても必ず実行される
+}
+printFruits();
